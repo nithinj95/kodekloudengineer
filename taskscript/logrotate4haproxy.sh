a8b0b4ee88f8eb6ebd5026a0dc26416f63f6aa6c
nithinj95 <nithingowada214@gmail.com>
2023-03-26 20:13:38 +0530

    changing the install package to squid

--- taskscript/logrotate4haproxy.sh
+++ taskscript/logrotate4haproxy.sh
@@ -9,31 +9,30 @@ else
     echo "Logrotate is already installed."
 fi
 
-# Check if haproxy is installed
-if ! command -v haproxy &> /dev/null
+# Check if squid is installed
+if ! command -v squid &> /dev/null
 then
-    # Install the haproxy package
-    sudo yum install haproxy -y
+    # Install the squid package
+    sudo yum install squid -y
 else
-    # Print a message indicating haproxy is already installed
-    echo "HAProxy is already installed."
+    # Print a message indicating squid is already installed
+    echo "squid is already installed."
 fi
 
-#Create a new logrotate configuration file for haproxy
-sudo tee /etc/logrotate.d/haproxy << EOF
-/path/to/haproxy.log {
+#Create a new logrotate configuration file for squid
+sudo tee /etc/logrotate.d/squid << EOF
+/var/log/squid.log {
     monthly
     rotate 3
     compress
     delaycompress
     notifempty
     missingok
-    create 644 haproxy haproxy
 }
 EOF
 
 # Check the syntax of the logrotate configuration file
-sudo logrotate -d /etc/logrotate.d/haproxy
+sudo logrotate -d /etc/logrotate.d/squid
 
-# Reload the haproxy service to pick up the changes
-sudo systemctl restart haproxy.service
+# Reload the squid service to pick up the changes
+sudo systemctl restart squid.service
